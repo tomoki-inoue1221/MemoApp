7c603ad1ccfd096da0c49cf2c55bd457b1faf384
井上知貴 <>
2020-07-11 20:14:48 +0900

    eslintを追加

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App. start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
